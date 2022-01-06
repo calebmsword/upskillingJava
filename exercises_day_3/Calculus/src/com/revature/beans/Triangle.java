557ba5fa31f57f58241e521294156a009b01690c
calebmsword <caleb.m.sword@gmail.com>
2022-01-06 13:39:21 -0500

    triangle class error handling

--- exercises_day_3/Calculus/src/com/revature/beans/Triangle.java
+++ exercises_day_3/Calculus/src/com/revature/beans/Triangle.java
@@ -11,12 +11,14 @@ public class Triangle extends Polygon {
     public Triangle(double dim1, double dim2, double dim3) {
         super(dim1, dim2);
 
-        if(dim3 < 0) {
-            System.out.println("WARNING: Triangle given angle less than 0. Angle automatically set to 0.");
-            this.dim3 = 0;
+        if (dim3 < 0) {
+            System.out.println("WARNING: Dimension 3 must be a value greater than 0. Angle automatically set to 1.");
+            this.dim3 = 1;
         } else if (dim3 > Math.PI/2) {
             System.out.println("WARNING: Triangle given angle greater than pi/2. Angle automatically set to pi/2.");
             this.dim3 = Math.PI/2;
+        } else if (dim3 == 0) {
+            System.out.println("WARNING: Dimension 3 cannot be 0. Angle automatically set to 1.");
         } else {
             this.dim3 = dim3;
         }
@@ -30,12 +32,14 @@ public class Triangle extends Polygon {
     }
 
     public void setDim3(double dim3) {
-        if(dim3 < 0) {
-            System.out.println("WARNING: Triangle given angle less than 0. Angle automatically set to 0.");
-            this.dim3 = 0;
+        if (dim3 < 0) {
+            System.out.println("WARNING: Dimension 3 must be a value greater than 0. Angle automatically set to 1.");
+            this.dim3 = 1;
         } else if (dim3 > Math.PI/2) {
             System.out.println("WARNING: Triangle given angle greater than pi/2. Angle automatically set to pi/2.");
             this.dim3 = Math.PI/2;
+        } else if (dim3 == 0) {
+            System.out.println("WARNING: Dimension 3 cannot be 0. Angle automatically set to 1.");
         } else {
             this.dim3 = dim3;
         }
