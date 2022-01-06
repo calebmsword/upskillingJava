2f5b9e64fa699aa5ec6b0abd22e5542368a7dd84
calebmsword <caleb.m.sword@gmail.com>
2022-01-06 13:47:24 -0500

    Main intentionally sets Triangle.dim3 to bad values

--- exercises_day_3/Calculus/src/com/revature/Main.java
+++ exercises_day_3/Calculus/src/com/revature/Main.java
@@ -14,5 +14,13 @@ public class Main {
         Triangle triangle = new Triangle(3,4,Math.PI/2);
         System.out.println("Area of triangle is: "+triangle.getArea());
         System.out.println("Perimeter of triangle is: "+triangle.getPerimeter());
+        System.out.println("We will get warning messages for instantiating Triangles whose dim3 field is greater than pi/2, 0, or negative.");
+        Triangle triangle2 = new Triangle(3,4,0);
+        Triangle triangle3 = new Triangle(3,4,-200000000);
+        Triangle triangle4 = new Triangle(3,4,200);
+        System.out.println("We will get warning messages for setting Triangle.dim3 to values greater than pi/2, 0, or negative.");
+        triangle2.setDim3(-200000);
+        triangle3.setDim3(0);
+        triangle4.setDim3(200);
     }
 }
