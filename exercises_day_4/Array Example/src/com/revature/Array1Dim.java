54047db3c65f6bc515247f6fcbf62f840052e78f
calebmsword <caleb.m.sword@gmail.com>
2022-01-07 16:06:53 -0500

    min and max in 1DArray

--- exercises_day_4/Array Example/src/com/revature/Array1Dim.java
+++ exercises_day_4/Array Example/src/com/revature/Array1Dim.java
@@ -1,6 +1,7 @@
 package com.revature;
 
 import java.util.Scanner;
+import java.lang.Math;
 
 public class Array1Dim {
     public static void main(String[] args) {
@@ -19,10 +20,21 @@ public class Array1Dim {
         }
 
         int sum = 0;
+        int min = marks[0];
+        int max = marks[0];
         System.out.println("Sum of all elements is:");
         for(int i=0; i < marks.length; i++) {
             sum += marks[i];
+            if (Math.min(marks[i], min) == marks[i]) {
+                min = marks[i];
+            }
+            if (Math.max(marks[i], max) == marks[i]) {
+                max = marks[i];
+            }
         }
         System.out.println(sum);
+
+        System.out.println("Min is: " + min);
+        System.out.println("Max is: " + max);
     }
 }
