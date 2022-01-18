c0c6c892a6bb107bf3a917c3201a9342137f034e
calebmsword <caleb.m.sword@gmail.com>
2022-01-18 15:24:17 -0500

    HibernateDemo

---
+++ week3/exercises_day_10/HibernateDemo/src/com/revature/App_02.java
@@ -0,0 +1,37 @@
+package com.revature;
+
+import com.revature.beans.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class App_02 {
+    public static void main(String[] args) {
+        StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+        Metadata metadata = new MetadataSources(ssr).getMetadataBuilder().build();
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+        Session session = sessionFactory.openSession();
+
+        Transaction transaction = session.beginTransaction();
+
+        Employee employee1 = new Employee("Caleb", "Sword");
+        Employee employee2 = new Employee("John", "Smith");
+        Employee employee3 = new Employee("My", "Mom");
+
+        session.save(employee1);
+        session.save(employee2);
+        session.save(employee3);
+
+        Employee employee = session.find(Employee.class, 2);
+        employee.setFirstName("My");
+        employee.setLastName("Mom");
+        employee.setFirstName("Your");
+        session.delete(employee);
+
+        transaction.commit();
+    }
+}
