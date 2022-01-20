09facc999e0826f44bb5661d5e0272fa21f58204
calebmsword <caleb.m.sword@gmail.com>
2022-01-20 15:46:45 -0500

    fixed .idea in LayerArchitecture_

---
+++ week3/exercises_day_13/LayeredArchitecture_/src/main/java/com/revature/app/App_01.java
@@ -0,0 +1,15 @@
+package com.revature.app;
+
+import com.revature.beans.Employee;
+import com.revature.controller.SaverController;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App_01 {
+    public static void main(String[] args) {
+        Employee employee = new Employee(101, "Caleb", "Sword", 99);
+        ApplicationContext ctxt = new ClassPathXmlApplicationContext("spring.xml");
+        SaverController controller = ctxt.getBean(SaverController.class);
+        controller.save(employee);
+    }
+}
