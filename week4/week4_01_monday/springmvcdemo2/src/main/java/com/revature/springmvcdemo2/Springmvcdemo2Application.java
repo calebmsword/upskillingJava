c63319ddf1b3b0ff05df1c06c5468bb5e2c41d4a
calebmsword <caleb.m.sword@gmail.com>
2022-01-24 13:19:58 -0500

    it works!

--- week4/week4_01_monday/springmvcdemo2/src/main/java/com/revature/springmvcdemo2/Springmvcdemo2Application.java
+++ week4/week4_01_monday/springmvcdemo2/src/main/java/com/revature/springmvcdemo2/Springmvcdemo2Application.java
@@ -2,8 +2,10 @@ package com.revature.springmvcdemo2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages={"com.revature"})
 public class Springmvcdemo2Application {
 
     public static void main(String[] args) {
